3073147ac0bee8264847691b77550505f0910bfc
Sooin Jung <sooin0514@gmail.com>
2025-02-13 23:34:25 +0000

    Implemented repositories

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,9 @@
 package com.example.repository;
+import com.example.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AccountRepository {
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+
 }
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,9 @@
 package com.example.repository;
+import com.example.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface MessageRepository {
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+
 }
